0dcda0c906a1b793dff21f180bab8fc8f57c313d
neustupov <neustupov@yandex.ru>
2020-09-18 17:07:08 +0300

    Обобщённые типы

---
+++ org/neustupov/rtti/FilledList.java
@@ -0,0 +1,28 @@
+package neustupov.rtti;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilledList<T> {
+
+  private Class<T> type;
+  public FilledList(Class<T> type){
+    this.type = type;
+  }
+  public List<T> create(int nElements){
+    List<T> result = new ArrayList<>();
+    try{
+      for (int i = 0; i < nElements; i++){
+        result.add(type.newInstance());
+      }
+    } catch (Exception e){
+      throw new RuntimeException(e);
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    FilledList<CountedInteger> fl = new FilledList<>(CountedInteger.class);
+    System.out.println(fl.create(15));
+  }
+}
