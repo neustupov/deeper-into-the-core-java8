c87cc48dd5c4a39c5a95f76697d52db4b155158c
neustupov <neustupov@yandex.ru>
2020-09-25 16:50:21 +0300

    Маски

--- org/neustupov/generics/wildcards/Wildcards.java
+++ org/neustupov/generics/wildcards/Wildcards.java
@@ -2,7 +2,33 @@ package neustupov.generics.wildcards;
 
 public class Wildcards {
 
-  static void rawArgs(Holder holder, Object args){
+  static void rawArgs(Holder holder, Object args) {
     holder.set(args);
+    holder.set(new Wildcards());
+    Object obj = holder.get();
+  }
+
+  static void unboundedArgs(Holder<?> holder, Object args){
+  }
+
+  static <T> T exact1(Holder<T> holder){
+    T t = holder.get();
+    return t;
+  }
+
+  static <T> T exact2(Holder<T> holder, T args){
+    holder.set(args);
+    T t = holder.get();
+    return t;
+  }
+
+  static <T> T wildSubtype(Holder<? extends T> holder, T args){
+    T t = holder.get();
+    return t;
+  }
+
+  static <T> void wildSpertype(Holder<? super T> holder, T args){
+    holder.set(args);
+    Object obj = holder.get();
   }
 }
