60d79ec12f81f0802e8cbc8a8f3ddf429188e48d
neustupov <neustupov@yandex.ru>
2020-09-29 13:23:38 +0300

    "Легковес" на абстрактном листе

---
+++ org/neustupov/collection/generators/abstractlist/CountingIntegerList.java
@@ -0,0 +1,25 @@
+package neustupov.collection.generators.abstractlist;
+
+import java.util.AbstractList;
+
+public class CountingIntegerList extends AbstractList<Integer> {
+
+  private int size;
+  public CountingIntegerList(int size){
+    this.size = size < 0 ? 0 : size;
+  }
+
+  @Override
+  public Integer get(int index) {
+    return index;
+  }
+
+  @Override
+  public int size() {
+    return size;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new CountingIntegerList(30));
+  }
+}
