99419b27fd2e03c763a4c4ec1e0fc003768601bf
Dmitry Neustupov <neustupov@yandex.ru>
2020-10-14 16:28:33 +0300

    Совместное использование ресурсов

---
+++ org/neustupov/multithreads/concurrency/EvenGenerator.java
@@ -0,0 +1,15 @@
+package neustupov.multithreads.concurrency;
+
+public class EvenGenerator extends IntGenerator{
+    private int currentEvenValue = 0;
+    @Override
+    public int next() {
+        ++currentEvenValue;
+        ++currentEvenValue;
+        return currentEvenValue;
+    }
+
+    public static void main(String[] args) {
+        EventChecker.test(new EvenGenerator());
+    }
+}
