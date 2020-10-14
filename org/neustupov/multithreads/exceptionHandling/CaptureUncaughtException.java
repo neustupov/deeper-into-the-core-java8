7b8f98d0b1765f895727994dbec8b83fd302fb3a
Dmitry Neustupov <neustupov@yandex.ru>
2020-10-14 15:37:57 +0300

    Перехват конкретных ошибок

---
+++ org/neustupov/multithreads/exceptionHandling/CaptureUncaughtException.java
@@ -0,0 +1,11 @@
+package neustupov.multithreads.exceptionHandling;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CaptureUncaughtException {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool(new HandlerThreadFactory());
+        exec.execute(new ExceptionThread());
+    }
+}
---
+++ org/neustupov/multithreads/exceptionHandling/ExceptionThread.java
@@ -0,0 +1,11 @@
+package neustupov.multithreads.exceptionHandling;
+
+public class ExceptionThread implements Runnable {
+    @Override
+    public void run() {
+        Thread t = Thread.currentThread();
+        System.out.println("run() by " + t);
+        System.out.println("eh= " + t.getUncaughtExceptionHandler());
+        throw new RuntimeException();
+    }
+}
