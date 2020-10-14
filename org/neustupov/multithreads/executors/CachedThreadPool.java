ab46a00a2bd8836486a2f76574664baf0ca18675
Dmitry Neustupov <neustupov@yandex.ru>
2020-10-14 09:56:37 +0300

    Экзекьюторы

---
+++ org/neustupov/multithreads/executors/CachedThreadPool.java
@@ -0,0 +1,16 @@
+package neustupov.multithreads.executors;
+
+import neustupov.multithreads.LiftOff;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPool {
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i = 0; i < 5; i++){
+            exec.execute(new LiftOff());
+        }
+    }
+}
