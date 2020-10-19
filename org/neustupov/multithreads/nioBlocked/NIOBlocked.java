93723a29ed64bb192140081aa967aadbb133f651
Dmitry Neustupov <neustupov@yandex.ru>
2020-10-19 17:03:52 +0300

    Прерывание заблокированного канала NIO

---
+++ org/neustupov/multithreads/nioBlocked/NIOBlocked.java
@@ -0,0 +1,30 @@
+package neustupov.multithreads.nioBlocked;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousCloseException;
+import java.nio.channels.ClosedByInterruptException;
+import java.nio.channels.SocketChannel;
+
+public class NIOBlocked implements Runnable{
+    private final SocketChannel sc;
+
+    public NIOBlocked(SocketChannel sc) {
+        this.sc = sc;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Waiting for read() in " + this);
+            sc.read(ByteBuffer.allocate(1));
+        } catch (ClosedByInterruptException e){
+            System.out.println("ClosedByInterruptedException");
+        } catch (AsynchronousCloseException e){
+            System.out.println("AsynchronousCloseException");
+        } catch (IOException e){
+            throw new RuntimeException();
+        }
+        System.out.println("Exit NIOBlocked");
+    }
+}
