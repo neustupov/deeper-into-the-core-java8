7f5811d10e85229e8684127144a3342a4311921b
Dmitry Neustupov <neustupov@yandex.ru>
2020-10-19 17:35:18 +0300

    Прерывание задачи, заблокированной по ReentrantLock

---
+++ org/neustupov/multithreads/interrupting/Blocked2.java
@@ -0,0 +1,13 @@
+package neustupov.multithreads.interrupting;
+
+public class Blocked2 implements Runnable{
+
+    BlockedMutex blocked = new BlockedMutex();
+
+    @Override
+    public void run() {
+        System.out.println("Waiting for f() in BlockedMutex");
+        blocked.f();
+        System.out.println("Broken out of blocked call");
+    }
+}
---
+++ org/neustupov/multithreads/interrupting/BlockedMutex.java
@@ -0,0 +1,22 @@
+package neustupov.multithreads.interrupting;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BlockedMutex {
+
+    private Lock lock = new ReentrantLock();
+
+    public BlockedMutex() {
+        lock.lock();
+    }
+
+    public void f() {
+        try {
+            lock.lockInterruptibly();
+            System.out.println("lock acquired in f()");
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted from lock in f()");
+        }
+    }
+}
