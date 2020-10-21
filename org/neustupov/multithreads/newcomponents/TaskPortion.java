aab1f2b72d58d2e4e7602b5d12c1500770c47a59
Dmitry Neustupov <neustupov@yandex.ru>
2020-10-21 17:39:33 +0300

    CountDownLatch

---
+++ org/neustupov/multithreads/newcomponents/TaskPortion.java
@@ -0,0 +1,38 @@
+package neustupov.multithreads.newcomponents;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class TaskPortion implements Runnable{
+    private static int counter = 0;
+    private final int id = counter++;
+    private static Random rand = new Random(47);
+    private final CountDownLatch latch;
+
+    public TaskPortion(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        try{
+            doWork();
+            latch.countDown();
+        } catch (InterruptedException e){
+            System.out.println("Interrupted");
+        }
+    }
+
+    public void doWork() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(rand.nextInt(2000));
+        System.out.println(this + " completed");
+    }
+
+    @Override
+    public String toString() {
+        return "TaskPortion{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ org/neustupov/multithreads/newcomponents/WaitingTask.java
@@ -0,0 +1,30 @@
+package neustupov.multithreads.newcomponents;
+
+import java.util.concurrent.CountDownLatch;
+
+public class WaitingTask implements Runnable{
+    private static int counter = 0;
+    private final int id = counter++;
+    private final CountDownLatch latch;
+
+    public WaitingTask(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        try{
+            latch.await();
+            System.out.println("Latch barrier passed for " + this);
+        } catch (InterruptedException e){
+            System.out.println(this + " interrupted");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "WaitingTask{" +
+                "id=" + id +
+                '}';
+    }
+}
