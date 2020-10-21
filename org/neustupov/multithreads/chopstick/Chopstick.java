4ff04e2f9990a0c14aa044b67ed8f1713a527e68
Dmitry Neustupov <neustupov@yandex.ru>
2020-10-21 16:35:42 +0300

    Взаимная блокировка

---
+++ org/neustupov/multithreads/chopstick/Chopstick.java
@@ -0,0 +1,15 @@
+package neustupov.multithreads.chopstick;
+
+public class Chopstick {
+    private boolean taken = false;
+    public synchronized void take() throws InterruptedException{
+        while (taken){
+            wait();
+        }
+        taken = true;
+    }
+    public synchronized void drop(){
+        taken = false;
+        notifyAll();
+    }
+}
