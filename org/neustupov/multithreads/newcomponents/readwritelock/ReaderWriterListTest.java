fa38ed5137898412190f37ce5d76875e98fb8da3
Dmitry Neustupov <neustupov@yandex.ru>
2020-10-27 17:38:52 +0300

    ReentrantReadWriteLock

---
+++ org/neustupov/multithreads/newcomponents/readwritelock/ReaderWriterListTest.java
@@ -0,0 +1,55 @@
+package neustupov.multithreads.newcomponents.readwritelock;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ReaderWriterListTest {
+
+    ExecutorService exec = Executors.newCachedThreadPool();
+    private final static int SIZE = 100;
+    private static Random rand = new Random(47);
+    private ReaderWriterList<Integer> list = new ReaderWriterList<>(SIZE, 0);
+
+    private class Writer implements Runnable {
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 20; i++) {
+                    list.set(i, rand.nextInt());
+                    TimeUnit.MILLISECONDS.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted");
+            }
+            System.out.println("Writer finished? shutting down");
+            exec.shutdownNow();
+        }
+    }
+
+    private class Reader implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (!Thread.interrupted()) {
+                    for (int i = 0; i < SIZE; i++) {
+                        list.get(i);
+                        TimeUnit.MILLISECONDS.sleep(1);
+                    }
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted");
+            }
+        }
+    }
+
+    public ReaderWriterListTest(int readers, int writers) {
+        for (int i = 0; i < readers; i++) {
+            exec.execute(new Reader());
+        }
+        for (int i = 0; i < writers; i++) {
+            exec.execute(new Writer());
+        }
+    }
+}
